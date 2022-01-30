61fb79c5cd8ea4fca4d1a5d32d2b4669d882d3ad
mac Ray <rztechdude@yahoo.com>
2022-01-29 21:36:48 -0500

    add local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,11 +57,11 @@ var createTaskEl = function(taskDataObj) {
   taskDataObj.id = taskIdCounter;
 
   tasks.push(taskDataObj);
+  saveTasks();
 
   // increase task counter for next unique id
   taskIdCounter++;
-  console.log(taskDataObj);
-  console.log(taskDataObj.status);
+ 
 };
 
 var createTaskActions = function(taskId) {
@@ -118,6 +118,7 @@ var completeEditTask = function(taskName, taskType, taskId) {
           tasks[i].type = taskType;
       }
   };
+  saveTasks();
 
   alert("Task Updated!");
 
@@ -166,7 +167,7 @@ var taskStatusChangeHandler = function(event) {
           tasks[i].status = statusValue;
       }
   }
-  console.log(tasks);
+  saveTasks();
 };
 
 var editTask = function(taskId) {
@@ -210,7 +211,12 @@ var deleteTask = function(taskId) {
   }
   // reassign tasks array to be the same as updatedTaskArr
   tasks = updatedTaskArr;
+  saveTasks();
 };
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
